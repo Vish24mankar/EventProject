0bd7a2c35eb7c21bdc3f47882358308cb9b6ab2f
Vish24mankar <vish24mankar@example.com>
2024-07-29 22:04:36 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-        <p>Event Frontend</p>
-        <h1>VS Events of Planners</h1>
+        {/* <p>Event Frontend</p> */}
+        {/* <h1>VS Events of Happiness</h1> */}
         <BrowserRouter>
             <Routes>
                 <Route path='' element={<Header />} />
